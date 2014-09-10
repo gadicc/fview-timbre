b3b8dd95deaa70611b8c7468176aae0f9cdc04e5
Gadi Cohen <dragon@wastelands.net>
2014-09-10 15:29:05 +0300

    fixed viewport, icons, some other CSS

--- fview-timbre.js
+++ fview-timbre.js
@@ -1,21 +1,21 @@
 if (Meteor.isClient) {
   Transform = famous.core.Transform;
 
-  Session.setDefault('menuOpen', false);
+  Session.set('menuOpen', false);
 
   // Translation for "main" page, depending on whether menu is open or not
   Template.famousInit.menuTranslate = function() {
-    return Session.get('menuOpen') ? [300,0,20] : [0,0,20];
+    return Session.get('menuOpen') ? [250,0,20] : [0,0,20];
   }
 
   // Set the transition to be used when translate= changes reactively
   Template.main.rendered = function() {
-    FView.from(this).modifierTransition = { curve: 'easeOut', duration: 500 };
+    FView.from(this).modifierTransition = { curve: 'easeInOut', duration: 500 };
   }
 
   // On click, toggle the menuOpen state / reactive Session variable
   Template.main.famousEvents({
-    'click': function(event, tpl) {
+    'mouseup': function(event, tpl) {
       Session.set('menuOpen', !Session.get('menuOpen'));
     }
   });
